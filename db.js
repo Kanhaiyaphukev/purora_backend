44a5d01002fc9378fbd76078c0f836a6e1065386
kanhaiyaphukev <Phuke.02kanhaiya@gmail.com>
2026-04-28 00:51:36 +0530

    Fixed MongoDB Atlas connection

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb+srv://phuke02kanhaiya_db_user:abhinay1989@kpsworkspace.ykeuwnz.mongodb.net/?appName=kpsWorkspace';
+const mongoURL = 'mongodb+srv://phuke02kanhaiya_db_user:abhinay1989@kpsworkspace.ykeuwnz.mongodb.net/mydb?retryWrites=true&w=majority&appName=kpsWorkspace';
 
 mongoose.connect(mongoURL)
     .then(() => {
